430f8abd74b3c5cb478ac081a2c3345673968544
Jakub Jurkian <jakub@jurkian.com>
2017-08-18 15:11:15 +0200

    Add /api and /api/users routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let morgan = require('morgan');
 
 let jwt = require('jsonwebtoken');
 let config = require('./config');
-let User = require('./app/models/user');
+let Users = require('./app/models/users');
 
 // Setup
 let port = process.env.PORT || 8080;
@@ -26,7 +26,20 @@ app.get('/', (req, res) => {
 })
 
 // API routes
-// ...
+let apiRoutes = express.Router();
+
+// Route: random message
+apiRoutes.get('/', (req, res) => {
+   res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// Route: return all users
+apiRoutes.get('/users', (req, res) => {
+   res.json(Users);
+});
+
+// Apply the routes to our app with prefix /api
+app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log(`Magic happens at http://localhost:${port}`);
